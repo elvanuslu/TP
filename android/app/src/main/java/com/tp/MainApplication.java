467d81ef5eb712febcd6cc8b12997e610167d832
Elvan <usluelvan@yahoo.com>
2019-05-28 08:08:56 +0300

    Map için Çalışma

--- android/app/src/main/java/com/tp/MainApplication.java
+++ android/app/src/main/java/com/tp/MainApplication.java
@@ -3,17 +3,15 @@ package com.tp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNTextInputMask.RNTextInputMaskPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,9 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
             new VectorIconsPackage(),
             new RNTextInputMaskPackage(),
+            new MapsPackage(),
             new RNGestureHandlerPackage(),
             new AsyncStoragePackage()
       );
