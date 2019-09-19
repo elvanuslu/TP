2275ac7b4cedc8cd7573c299710146df8b04ee87
Elvan <usluelvan@yahoo.com>
2019-09-19 17:15:53 +0300

    Firebase Eklendi ve Düzgün Compile Ediyor.

--- android/app/src/main/java/com/turkiyepetroller/TP/MainApplication.java
+++ android/app/src/main/java/com/turkiyepetroller/TP/MainApplication.java
@@ -3,6 +3,7 @@ package com.turkiyepetroller.TP;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.RNTextInputMask.RNTextInputMaskPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new GeolocationPackage(),
             new RNFusedLocationPackage(),
                        new RNTextInputMaskPackage(),
